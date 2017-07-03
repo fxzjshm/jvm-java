258f8434b06950f5a60617d6c6048d5c51d0c677
fxzjshm <fxzjshm@163.com>
2017-07-03 12:31:28 +0800

    Add tests but not tested

---
+++ test-gwt/src/io/github/fxzjshm/jvm/java/test/gwt/client/GwtModule.java
@@ -0,0 +1,12 @@
+package io.github.fxzjshm.jvm.java.test.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GwtModule implements EntryPoint{
+
+    @Override
+    public void onModuleLoad() {
+        
+    }
+
+}
